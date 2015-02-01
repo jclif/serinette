18aea6d4725a38b7d8eeb984becbcece2ee0303d
Jared Clifton <jaredcraigclifton@gmail.com>
2015-01-31 23:29:22 -0500

    adds a noise test

--- lib/noise.rb
+++ lib/noise.rb
@@ -1,2 +1,11 @@
+require 'wavefile'
+
 class Noise
+
+  attr_accessor :wavefile
+
+  def initialize
+    @wavefile = Wavefile.new
+  end
+
 end
--- spec/noise_spec.rb
+++ spec/noise_spec.rb
@@ -1,9 +1,12 @@
 require 'noise'
+require 'wavefile'
 
 describe Noise do
 
   subject(:noise) { Noise.new() }
 
-  it "should have a play method"
+  it "should have a wavefile" do
+    expect(noise.wavefile).to be_a Wavefile
+  end
 
 end
