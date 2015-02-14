845d30fedef1051b46961dfa0dc15833c7ff0325
Jared Clifton <jaredcraigclifton@gmail.com>
2015-02-14 02:41:30 -0500

    ignore .inch dir and add documentation

--- lib/serinette.rb
+++ lib/serinette.rb
@@ -14,8 +14,10 @@ module Serinette
 
   OUTPUT_FILE_NAME = 'tmp/output.wav'
   TMP_DIR = "./tmp/"
+  SOUND_FILE_TYPE = '.wav'
 
   def self.clean
+    # Removes all files in TMP_DIR of type SOUND_FILE_TYPE
     FileUtils.rm_rf(Dir.glob("#{TMP_DIR}*.wav"))
   end
 
--- lib/serinette/effect.rb
+++ lib/serinette/effect.rb
@@ -1,14 +1,20 @@
 class Effect
+  # Used to get subclasses and generate sox commands
 
   include Descendents
 
   def self.get_effects(num)
+    # Gets a specified number of Effect subclasses.
+    #
+    # @param num [Fixnum] Number of effects
+    # @return [Array] An array of Effect subclasses
     Array.new(num).map do |el|
       el = Effect.descendents.sample.new
     end
   end
 
   def to_sox_key_pair
+    # Gets a key pair for a given sox effect and its args
     return {get_root_command => get_options}
   end
 
