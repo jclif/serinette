9fc3112a473784418b917af691f1b172d809493a
Jared Clifton <jaredcraigclifton@gmail.com>
2015-02-16 01:43:44 -0500

    adding tests for sox options

--- lib/mixins/sox_options.rb
+++ lib/mixins/sox_options.rb
@@ -5,10 +5,10 @@ module SoxOptions
   # @param config [Array] Sox effect options config
   #
   # @return [string] Randomized effect hash to be used with Sox::Cmd.set_effects
-  def self.randomize_effect_options_as_string(config)
-    config.map do |o|
+  def self.randomize_effect_options_as_string(configs)
+    configs.map do |o|
       stringify_option(o)
-    end.join(' ')
+    end.compact.join(' ')
   end
 
   # Takes an individual option and checks for type, (if flag, its either on of
@@ -33,6 +33,7 @@ module SoxOptions
   def self.stringify_option(option)
     case option[:type]
     when :flag
+      return [true, false].sample ? option[:value] : nil
     when :trait
     else
       fail Serinette::Error, '#stringify_option requires type = flag/trait'
--- spec/mixins/sox_options_spec.rb
+++ spec/mixins/sox_options_spec.rb
@@ -8,9 +8,43 @@ describe SoxOptions do
     end
   end
 
-  context 'a method #randomize_effect_options_as_string' do
+  context 'a method #stringify_option' do
     it 'should exist' do
-      expect(SoxOptions).to respond_to(:randomize_effect_options_as_string)
+      expect(SoxOptions).to respond_to(:stringify_option)
+    end
+
+    it 'should return correct string when given type: :flag' do
+      good_option1 = {
+        type: :flag,
+        name: 'wet-only',
+        value: '-w'
+      }
+
+      allow_any_instance_of(Array).to receive(:sample).and_return(true)
+      expect(SoxOptions.stringify_option(good_option1)).to eq('-w')
+    end
+
+    it 'should return correct string when given type: :trait' do
+      good_option2 = {
+        type: :trait,
+        name: 'reverberance',
+        range: (0..100),
+        default: 50
+      }
+
+      result =SoxOptions.stringify_option(good_option2)
+
+      expect(result).not_to be_nil
+      expect(result.to_i).to be_between(0, 100)
+    end
+
+    it 'should fail when type is bad' do
+      bad_option = {
+        type: :bad,
+        name: 'wet-only',
+        value: '-w'
+      }
+      expect{ SoxOptions.stringify_option(bad_option) }.to raise_error(Serinette::Error)
     end
   end
 end
