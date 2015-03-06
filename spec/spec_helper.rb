611865de82515242391b9e39e159f78e37f7422c
Jared Clifton <jaredcraigclifton@gmail.com>
2015-03-06 18:22:05 -0500

    Add Logic for Sending Stdout to Test Log File

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,3 +9,20 @@ require 'serinette'
 # Custom Matchers
 d = File.expand_path(File.join(File.dirname(__FILE__), 'support', '**', '*.rb'))
 Dir[d].each { |f| require f }
+
+# Ensures that stdout is directed into dev/null.txt during tests
+RSpec.configure do |config|
+  original_stderr = $stderr
+  original_stdout = $stdout
+
+  config.before(:all) do
+    dev = File.join(File.dirname(__FILE__), 'dev', 'log.txt')
+    $stderr = File.new(dev, 'w')
+    $stdout = File.new(dev, 'w')
+  end
+
+  config.after(:all) do
+    $stderr = original_stderr
+    $stdout = original_stdout
+  end
+end
