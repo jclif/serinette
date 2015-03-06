5a063e77e9a2d701ca6a67e1a0eae3fccd621f06
Jared Clifton <jaredcraigclifton@gmail.com>
2015-03-06 18:23:49 -0500

    Dont Track Test Logs and Fixes CLI Test

--- spec/serinette/cli_spec.rb
+++ spec/serinette/cli_spec.rb
@@ -4,10 +4,10 @@ describe Serinette::CLI do
   let(:argument_error) { Thor::RequiredArgumentMissingError }
 
   it 'should throw error when invalid command is given' do
-    expect { Seshat::CLI.new.invoke :not_valid }.to raise_error(argument_error)
+    expect { Serinette::CLI.new.invoke :not_valid }.to raise_error(argument_error)
   end
 
   it 'should run valid command' do
-    expect { Seshat::CLI.new.invoke :generate }.not_to raise_error
+    expect { Serinette::CLI.new.invoke :generate }.not_to raise_error
   end
 end
